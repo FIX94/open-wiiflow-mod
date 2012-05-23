76c4e48cc42f6240095914fddc58ff1c4e6ac945
fix94.1@gmail.com <fix94.1@gmail.com@5d90fa2f-112f-2c21-deeb-19951a5ba7ed>
2012-05-23 16:31:49 +0000

    -added source and binary for wii-mednafen mod1 -added source for wiiflow homebrew channel stub alternative

--- source/homebrew/homebrew.h
+++
@@ -1,8 +0,0 @@
-#ifndef _BOOTHOMEBREW_H_
-#define _BOOTHOMEBREW_H_
-
-int BootHomebrew(bool wiiflow_stub);
-void AddBootArgument(const char * arg);
-int LoadHomebrew(const char * filepath);
-
-#endif
