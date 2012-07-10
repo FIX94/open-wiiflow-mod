2c4a7d7f4cd56b85b92d4b4c76354ee44df821be
fix94.1@gmail.com <fix94.1@gmail.com@5d90fa2f-112f-2c21-deeb-19951a5ba7ed>
2012-07-10 00:24:34 +0000

    -updated fceugx, snes9xgx and vbagx to mod11, updated to latest svn that they dont annoy people with new updates available message

--- branches/Plugins/fceugc/source/fceugx.h
+++ branches/Plugins/fceugc/source/fceugx.h
@@ -17,7 +17,7 @@
 #include "fceultra/driver.h"
 
 #define APPNAME			"FCE Ultra GX"
-#define APPVERSION		"3.2.9"
+#define APPVERSION		"3.3.1"
 #define APPFOLDER 		"fceugx"
 #define PREF_FILE_NAME	"settings_fceugx.xml"
 
@@ -127,6 +127,8 @@ extern int ExitRequested;
 extern char appPath[];
 extern char loadedFile[];
 extern int frameskip;
+extern int fskip;
+extern int fskipc;
 extern int turbomode;
 extern bool romLoaded;
 
